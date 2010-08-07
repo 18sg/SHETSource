817de9bea2fa1338ddd6d88506ff01a07e50bb44
Thomas Nixon <tomjnixon@googlemail.com>
2010-08-07 14:55:19 +0100

    Added example serial client.

---
+++ shet_client/serial_test.py
@@ -0,0 +1,49 @@
+from shet.client import ShetClient
+from twisted.internet import reactor
+from twisted.internet import defer
+
+from twisted.internet.serialport import SerialPort
+from twisted.internet.protocol import Protocol
+
+
+class ArduinoTest(Protocol):
+	"""A protocol that will be passed data from the serial port.
+	(It's just a protocol though - it could be connected to anything)
+	"""
+	def __init__(self, shet):
+		#Protocol.__init__(self)
+		self.shet = shet
+	
+	
+	def dataReceived(self, data):
+		"""Called by twisted with a string read from the serial port.
+		"""
+		for c in data:
+			print "received: %s" % hex(ord(c))
+			# Fire the shet event.
+			self.shet.event(hex(ord(c)))
+
+
+
+class TestShetClient(ShetClient):
+	"""A shet client that reads from the serial port, and fires event '/test'
+	whenever data is received.
+	"""
+	def __init__(self):
+		ShetClient.__init__(self)
+		# create an instance of the protocol, with a reference to this
+		
+		arduino = ArduinoTest(self)
+		# connect it to the serial port
+		SerialPort(arduino, "/dev/ttyUSB0", reactor)
+		
+		# standard shet stuff.
+		self.event = self.add_event("test")
+
+
+def main():
+	# Create the shet client and run it.
+	TestShetClient().run("localhost")
+
+if __name__ == "__main__":
+	main()
