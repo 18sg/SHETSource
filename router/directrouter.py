167ef187b4d1b5214772ad71f9d423db8b88f2e3
Thomas Nixon <tom@tomn.co.uk>
2012-04-26 22:46:45 +0100

    Add argument parsing for direct router.

--- router/directrouter.py
+++ router/directrouter.py
@@ -8,8 +8,17 @@ from shet.client import ShetClient
 from client import Client
 
 
+def get_args():
+	import argparse
+	parser = argparse.ArgumentParser(description="SHETSource direct router.")
+	parser.add_argument("device", default="/dev/arduino", nargs="?",
+	                    help="The /dev device of the arduino. default=/dev/arduino")
+	return parser.parse_args()
+
 
 if __name__ == "__main__":
+	args = get_args()
+	
 	# Connect to SHET.
 	shet = ShetClient()
 	shet.install()
@@ -18,6 +27,6 @@ if __name__ == "__main__":
 	client = Client(shet)
 	
 	# ...and finally connect the client to the serial port.
-	SerialPort(client, "/dev/arduino", reactor, baudrate=57600)
+	SerialPort(client, args.device, reactor, baudrate=57600)
 	
 	reactor.run()
